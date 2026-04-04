1d4c7876c0d722a8f0236179bbf7c3e48a3be7de
gepres <genaropretill@gmail.com>
2026-04-03 19:27:35 -0500

    feat: agregar image a prompt stream

--- src/gemini/gemini.controller.ts
+++ src/gemini/gemini.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Res, UploadedFiles, UseInterceptors } from '@nestjs/common';
 import { GeminiService } from './gemini.service';
 import { BasicPromptDto } from './dtos/basic-prompt.dto';
 import type { Response } from 'express';
+import { FilesInterceptor } from '@nestjs/platform-express';
 
 @Controller('gemini')
 export class GeminiController {
@@ -14,10 +15,17 @@ export class GeminiController {
   }
 
   @Post('/basic-prompt-stream')
+  @UseInterceptors(FilesInterceptor('files'))
   async basicPromptStream(
     @Body() basicPromptDto: BasicPromptDto,
     @Res() res: Response,
+    @UploadedFiles() files: Array<Express.Multer.File>
   ) {
+
+    // console.log({files});
+
+    basicPromptDto.files = files ?? []
+    
     const stream = await this.geminiService.basicPromptStream(basicPromptDto);
 
     res.setHeader('Content-Type', 'text/plain');
--- src/gemini/use-cases/basic-prompt-stream.use-case.ts
+++ src/gemini/use-cases/basic-prompt-stream.use-case.ts
@@ -1,4 +1,4 @@
-import { GoogleGenAI } from "@google/genai";
+import { createPartFromUri, createUserContent, GoogleGenAI } from "@google/genai";
 import { BasicPromptDto } from "../dtos/basic-prompt.dto";
 
 interface Options {
@@ -7,6 +7,22 @@ interface Options {
 }
 
 export const basicPromptStreamUseCase = async (ai: GoogleGenAI, basicPromptDto: BasicPromptDto, options?: Options) => {
+
+    const { prompt,files = [] } = basicPromptDto;
+
+    // const firstImage = files?.[0]!;
+
+    // const image = await ai.files.upload({
+    //     file: new Blob([firstImage.buffer as any], { type: firstImage.mimetype }),
+        
+    // })
+
+    const images = await Promise.all(files.map( file => {
+        return  ai.files.upload({
+            file: new Blob([file.buffer as any], { type: file.mimetype.includes('image') ? file.mimetype : 'image/jpeg'  }),
+        })
+    }))
+    
 
     const {
         model = 'gemini-2.5-flash',
@@ -18,7 +34,15 @@ export const basicPromptStreamUseCase = async (ai: GoogleGenAI, basicPromptDto: 
 
     const response = await ai.models.generateContentStream({
         model,
-        contents: basicPromptDto.prompt,
+        // contents: basicPromptDto.prompt,
+        contents:[
+            createUserContent([
+                prompt,
+                // imagenes o archivo
+                // createPartFromUri(images.uri ?? '', image.mimeType ?? '')
+                ...images.map(image => createPartFromUri(image.uri ?? '', image.mimeType ?? ''))
+            ])
+        ],
         config: {
             systemInstruction
         }
